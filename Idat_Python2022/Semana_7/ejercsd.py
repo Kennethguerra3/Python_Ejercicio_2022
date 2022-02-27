d262c666aa717c52b6f56d1d1298bc735e0e411b
Kennethguerra3 <kenneth.guerras@gmail.com>
2022-02-26 20:15:55 -0500

    o

--- Idat_Python2022/Semana_7/ejercsd.py
+++ Idat_Python2022/Semana_7/ejercsd.py
@@ -1,1 +1,13 @@
 #Practicxa de ejercicio
+Edad=int(input("Ingrese la edad: "))
+if Edad >=40:
+    Bono=300
+elif Edad >=50:
+    Bono=400
+elif Edad >=80:
+    Bono=600
+elif Edad >= 90:
+    Bono=700
+else:
+    print("Lamento No hay bono para este rango de edad.!")
+
