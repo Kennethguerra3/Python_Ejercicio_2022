c2652ba0298970c627ed8e63d9f09ab19cac41ba
Kennethguerra3 <kenneth.guerras@gmail.com>
2022-02-28 16:30:27 -0500

    semana 6, udpdate 6.py

--- Idat_Python2022/Semana_6/6.py
+++ Idat_Python2022/Semana_6/6.py
@@ -1,16 +1,15 @@
 edad = int(input("Edad: "))
-
 if edad <= 3:
-   print("Bebe")
+   print("Bebe con ganas de crecer")
 elif edad <= 10:
-   print("Niño")
+   print("Niño pero muy inteligente")
 elif edad <= 15:
-   print("Puber")
+   print("Puber con capacidad intelectual")
 elif edad <= 18:
-   print("Adolescente")
+   print("Adolescente futuro cidadanao maduro")
 elif edad <= 30:
-   print("Joven")
+   print("Joven que aporta a la sociedad")
 elif edad <= 65:
-   print("Adulto")
+   print("Adulto esta sembrando")
 else:
-   print("Adulto mayor")
+   print("Adulto mayor da consejos de vida")
