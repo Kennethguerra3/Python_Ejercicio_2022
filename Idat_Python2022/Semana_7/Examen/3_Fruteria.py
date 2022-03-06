9f3ae53222405ea191bffa43c1716181847ca935
Kennethguerra3 <kenneth.guerras@gmail.com>
2022-03-05 23:28:37 -0500

    update python sem 07 examen
    update python sem 07 examen

--- Idat_Python2022/Semana_7/Examen/3_Fruteria.py
+++ Idat_Python2022/Semana_7/Examen/3_Fruteria.py
@@ -5,32 +5,35 @@
             5.01 – 10	        |       15 %
             10.01 en adelante	|       20%   """
 print("=======================================")
-print("PESAJE | DESCUENTO | PAGO TOAL")
+print("PESAJE | DESCUENTO | PAGO TOTAL")
 print("=======================================")
-print("                                       ")
+print("        |FRUTITA.COM|                  ")
+
+cantidad = float(input("Ingrese kilos a comprar : "))
+precio = float(input("Ingrese Precio por kilo: "))
+
+# Calculo para la condiciones de descuento del if y elif:
+
 # Ingresar la variables para el input:
-cantidad=float(input("Ingrese kilos a comprar : "))
-precio=float(input("Ingrese precio por kilo: "))
-
-#Calculo para la condiciones de descuento del if y elif:
-if cantidad <=2:
-    total_neto=precio*cantidad
+if cantidad <= 2:
+    total_neto = precio * cantidad
     print("Total a pagar", total_neto)
-elif
-    cantidad >= 2.01 and 5:
-    total_neto=precio*cantidad
-    descueto=total_neto * 0.10
-    Total_Pagar=total_neto-descueto
-    print("Total Pagar Manzana", Total_Pagar)
-elif cantidad ==5.01 and 10:
-    total_neto=precio*cantidad
-    descueto=total_neto * 0.15
-    Total_Pagar=total_neto-descueto
+elif cantidad >= 2.01 and 5:
+    total_neto = precio * cantidad
+    descuento = total_neto * 0.10
+    Total_Pagar = total_neto - descuento
     print("Total Pagar Manzana", Total_Pagar)
 
-else:
-    cantidad >= 10.01:
-    total_neto =cantidad*precio
-    descueto = total_neto * 0.20
-    Total_Pagar = total_neto - descueto
+elif cantidad == 5.01 and 10:
+    total_neto = precio * cantidad
+    descuento = total_neto * 0.15
+    Total_Pagar = total_neto - descuento
     print("Total Pagar Manzana", Total_Pagar)
+
+elif cantidad >= 10.01:
+    total_neto = cantidad * precio
+    descuento = total_neto * 0.20
+    Total_Pagar = total_neto - descuento
+    print("Total Pagar Manzana", Total_Pagar)
+else:
+    print("No hay descuento para este pesaje!")
